799b86415d55a78f8591ccd40d3b5a759aa2ecf8
ragul08 <87308511+ragul08@users.noreply.github.com>
2021-08-06 15:59:40 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,11 @@
+var ragul
+function setup() {
+  createCanvas(400,400);
+  ragul=new Student("ragul",8,14)
+  ragul.show()
+}
+function draw() 
+{
+  background(30);
+  
+}
---
+++ student.js
@@ -0,0 +1,19 @@
+
+class Student
+{
+    constructor(name,grade,age){
+        this.name=name
+        this.grade=grade
+        this.age=age
+    }
+   show(){
+       console.log(this.name)
+       console.log(this.grade)
+       console.log(this.age)
+
+   }
+
+}
+
+
+
